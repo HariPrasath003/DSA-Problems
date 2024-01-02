b908f4438e31f08820c844a2c70db07d0754eef2
Hariprasath R <hariprasath1115@gmail.com>
2024-01-02 20:20:02 +0530

    Day 2 of 100DayOfCode

---
+++ Day2/Subarray.java
@@ -0,0 +1,102 @@
+// Given an unsorted array A of size N that contains only non negative integers, find a continuous sub-array that adds to a given number S and return the left and right index(1-based indexing) of that subarray.
+
+// In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
+
+// Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
+
+// Example 1:
+
+// Input:
+// N = 5, S = 12
+// A[] = {1,2,3,7,5}
+// Output: 2 4
+// Explanation: The sum of elements 
+// from 2nd position to 4th position 
+// is 12.
+// Example 2:
+
+// Input:
+// N = 10, S = 15
+// A[] = {1,2,3,4,5,6,7,8,9,10}
+// Output: 1 5
+// Explanation: The sum of elements 
+// from 1st position to 5th position
+// is 15.
+
+
+
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        boolean found = false;
+        int st =0; int  end =0; 
+        int sum =0;
+        
+        for(int i=0 ; i<n; i++)
+        {
+            sum +=arr[i];
+            
+            if(sum >=s)
+            {
+                end = i;
+                while(s<sum && st<end)
+                {
+                    sum -= arr[st++];
+                }
+            }
+            if(sum == s)
+            {
+                al.add(st+1); al.add(end +1);
+                found =true;
+                break;
+            }
+        }
+        if(!found)
+        al.add(-1);
+        
+        return al;
+    }
+}
