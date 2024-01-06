852519d8d906867885b08e0c065c459955ce4c28
Hariprasath R <hariprasath1115@gmail.com>
2024-01-06 19:19:46 +0530

    Day6 of 100DayOfCode

---
+++ Day6/Sortanarrayof0sand1sand2s.java
@@ -0,0 +1,91 @@
+// Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+// Example 1:
+
+// Input: 
+// N = 5
+// arr[]= {0 2 1 2 0}
+// Output:
+// 0 0 1 2 2
+// Explanation:
+// 0s 1s and 2s are segregated 
+// into ascending order.
+// Example 2:
+
+// Input: 
+// N = 3
+// arr[] = {0 1 0}
+// Output:
+// 0 0 1
+// Explanation:
+// 0s 1s and 2s are segregated 
+// into ascending order.
+
+
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+         int low =0 , mid = 0 , high = n-1;
+        
+      while (mid <= high) {
+            if (a[mid] == 0) {
+              
+                int temp = a[low];
+                a[low] = a[mid];
+                a[mid] = temp;
+
+                low++;
+                mid++;
+            } else if (a[mid] == 1) {
+                mid++;
+            } else {
+               
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+
+                high--;
+            }
+        }
+    }   
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
