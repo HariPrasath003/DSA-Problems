5665f20a59c7d945a60ac63cc3948f865f7516a5
Hariprasath R <hariprasath1115@gmail.com>
2024-02-03 18:25:45 +0530

    Day 33 of 100DayOfCode

--- Day33/ SearchinRotatedSortedArray.java
+++ Day33/ SearchinRotatedSortedArray.java
@@ -30,4 +30,39 @@
 // nums is an ascending array that is possibly rotated.
 // -104 <= target <= 104
 
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid]) {
+
+                    right = mid - 1;
+                } else {
+
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } 
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
 
