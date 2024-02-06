cc2d95668d30e52f88300c5c18d8b6569e49abf9
Hariprasath R <hariprasath1115@gmail.com>
2024-02-06 20:31:38 +0530

    Day 37 of 100DayOfCode

---
+++ Day37/SmallestPositiveMissingNumber.java
@@ -0,0 +1,97 @@
+// You are given an array arr[] of N integers. The task is to find the smallest positive number missing from the array.
+
+// Note: Positive number starts from 1.
+
+// Example 1:
+
+// Input:
+// N = 5
+// arr[] = {1,2,3,4,5}
+// Output: 6
+// Explanation: Smallest positive missing 
+// number is 6.
+// Example 2:
+
+// Input:
+// N = 5
+// arr[] = {0,-10,1,3,-20}
+// Output: 2
+// Explanation: Smallest positive missing 
+// number is 2.
+// Your Task:
+// The task is to complete the function missingNumber() which returns the smallest positive missing number in the array.
+
+// Expected Time Complexity: O(N).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 1 <= N <= 106
+// -106 <= arr[i] <= 106
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+		for (int i = 0; i < size; i++) {
+            while (arr[i] > 0 && arr[i] <= size && arr[arr[i] - 1] != arr[i]) {
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return size + 1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) throws IOException
+    {
+
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out=new PrintWriter(System.out);
+		
+        //taking testcases
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    
+		    //input number n
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] arr=new int[n];
+			String s[]=in.readLine().trim().split(" ");
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=Integer.parseInt(s[i]);
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			out.println(missing);
+		}
+		out.close();
+    }
+}
+
+
+// } Driver Code Ends
