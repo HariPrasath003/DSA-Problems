01cab26df845e1ab8fc3569e3cedf505a4bed1c1
Hariprasath R <hariprasath1115@gmail.com>
2024-03-22 19:53:14 +0530

    Day 82 of 100DayOfCode

---
+++ day82/PalindromeLinkedList.java
@@ -0,0 +1,80 @@
+// Given the head of a singly linked list, return true if it is a 
+// palindrome
+//  or false otherwise.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,2,1]
+// Output: true
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: false
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 105].
+// 0 <= Node.val <= 9
+ 
+
+// Follow up: Could you do it in O(n) time and O(1) space?
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode secondHalf = reverseLinkedList(slow.next);
+        
+        ListNode p1 = head;
+        ListNode p2 = secondHalf;
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        return true;
+    }
+    
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
+// Time: O(N)
+// Space: O(1)
