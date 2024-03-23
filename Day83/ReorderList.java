02874d25165f3354b8e6a831553f196c91e82b8e
Hariprasath R <hariprasath1115@gmail.com>
2024-03-23 20:06:48 +0530

    Day 83 of 100DayOfCode

---
+++ Day83/ReorderList.java
@@ -0,0 +1,86 @@
+// You are given the head of a singly linked-list. The list can be represented as:
+
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4]
+// Output: [1,4,2,3]
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [1,5,2,4,3]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 5 * 104].
+// 1 <= Node.val <= 1000
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        
+        if (head == null || head.next == null || head.next.next == null)
+            return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+
+        secondHalf = reverseList(secondHalf);
+        mergeLists(head, secondHalf);
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+    private void mergeLists(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            ListNode l1Next = l1.next;
+            ListNode l2Next = l2.next;
+
+            l1.next = l2;
+            l2.next = l1Next;
+
+            l1 = l1Next;
+            l2 = l2Next;
+        }
+
+    }
+}
+
