2203624e91b9fb1c21e4e52853e3b39f0a719c99
Hariprasath R <hariprasath1115@gmail.com>
2024-04-20 19:55:22 +0530

    Day 111

---
+++ Day111/NthFibonacciNumber.java
@@ -0,0 +1,69 @@
+// Given a positive integer n, find the nth fibonacci number. Since the answer can be very large, return the answer modulo 1000000007.
+
+// Note: for the reference of this question take first fibonacci number to be 1.
+
+// Example 1:
+
+// Input: 
+// n = 2
+// Output: 
+// 1 
+// Explanation: 
+// 1 is the 2nd number of fibonacci series.
+// Example 2:
+
+// Input: 
+// n = 5
+// Output: 
+// 5
+// Explanation: 
+// 5 is the 5th number of fibonacci series.
+// Your Task:  
+// You dont need to read input or print anything. Complete the function nthFibonacci() which takes n as input parameter and returns nth fibonacci number.
+
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(n)
+
+// Constraints:
+// 1<= n <=105
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthFibonacci(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution {
+    static int nthFibonacci(int n) {
+        
+        if (n == 1 || n == 2)
+            return 1;
+
+        int[] fib = new int[n + 1];
+        fib[1] = 1;
+        fib[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            fib[i] = (fib[i - 1] + fib[i - 2]) % 1000000007;
+        }
+
+        return fib[n];
+    }
+}
---
+++ Day111/nCr.java
@@ -0,0 +1,64 @@
+// Given two integers n and r, find nCr. Since the answer may be very large, calculate the answer modulo 109+7.
+
+// Example 1:
+
+// Input: n = 3, r = 2
+// Output: 3
+// Explaination: 3C2 = 3. 
+// Example 2:
+
+// Input: n = 2, r = 4
+// Output: 0
+// Explaination: r is greater than n.
+// Your Task:
+// You do not need to take input or print anything. Your task is to complete the function nCr() which takes n and r as input parameters and returns nCr modulo 109+7..
+
+// Expected Time Complexity: O(n*r)
+// Expected Auxiliary Space: O(r)
+
+// Constraints:
+// 1 ≤ n ≤ 1000
+// 1 ≤ r ≤ 800
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String read[] = in.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(read[0]);
+            int r = Integer.parseInt(read[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nCr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution{
+    static int nCr(int n, int r){
+        
+        int[] arr = new int[r+1];
+        arr[0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=Math.min(i,r);j>0;j--){
+                
+                arr[j] = (arr[j] + arr[j-1])%(1000000007);
+            }
+        }
+        return arr[r];
+    }
+}
+   
