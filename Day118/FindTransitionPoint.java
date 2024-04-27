8c8e42b1a2439821cdb21e9881c509c9f3b9414c
Hariprasath R <hariprasath1115@gmail.com>
2024-04-27 17:57:43 +0530

    Day 118

--- Day118/FindTransitionPoint.java
+++ Day118/FindTransitionPoint.java
@@ -27,25 +27,26 @@
 // 0 ≤ arr[i] ≤ 1
 
 //{ Driver Code Starts
-    import java.util.*;
 
-    class Sorted_Array {
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-            int T = sc.nextInt();
-            while (T > 0) {
-                int n = sc.nextInt();
-                int arr[] = new int[n];
-                for (int i = 0; i < n; i++) {
-                    arr[i] = sc.nextInt();
-                }
-                Solution obj = new Solution();
-                System.out.println(obj.transitionPoint(arr, n));
-                T--;
+import java.util.*;
+
+class Sorted_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
             }
+            Solution obj = new Solution();
+            System.out.println(obj.transitionPoint(arr, n));
+            T--;
         }
     }
-    // } Driver Code Ends
+}
+// } Driver Code Ends
     
     
 class Solution {
