908446a0ff7d88808e7ae6d04db66808ccee44c7
Hariprasath R <hariprasath1115@gmail.com>
2024-05-22 17:03:39 +0530

    Day 143 Done

---
+++ Day143/Solution.java
@@ -0,0 +1,63 @@
+// Given a string s, partition s such that every 
+// substring
+//  of the partition is a 
+// palindrome
+// . Return all possible palindrome partitioning of s.
+
+ 
+
+// Example 1:
+
+// Input: s = "aab"
+// Output: [["a","a","b"],["aa","b"]]
+// Example 2:
+
+// Input: s = "a"
+// Output: [["a"]]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 16
+// s contains only lowercase English letters.
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+
+        List<List<String>> result = new ArrayList<>();
+
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+
+        if (start == s.length()) {
+
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+
+            if (isPalindrome(s, start, end - 1)) {
+
+                path.add(s.substring(start, end));
+                backtrack(s, end, path, result);
+
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Time complexity: O(N * 2^N)
+// Space complexity: O(N)
