fa3ef11aadd6556ed6e52a3f402badf1161eeaf0
Hariprasath R <hariprasath1115@gmail.com>
2024-05-24 11:16:12 +0530

    Day 145 Done

--- Day145/Solution.java
+++ Day145/Solution.java
@@ -6,8 +6,6 @@ package Day145;
 
 // It is not necessary to use all characters in letters and each letter can only be used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
 
- 
-
 // Example 1:
 
 // Input: words = ["dog","cat","dad","good"], letters = ["a","a","c","d","d","d","g","o","o"], score = [1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0]
