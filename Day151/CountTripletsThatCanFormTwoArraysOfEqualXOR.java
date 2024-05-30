10f5738884d389e17ba5eb27dc009a9ba6818039
Hariprasath R <hariprasath1115@gmail.com>
2024-05-30 17:34:26 +0530

    Day 151 Done

--- Day151/CountTripletsThatCanFormTwoArraysOfEqualXOR.java
+++ Day151/CountTripletsThatCanFormTwoArraysOfEqualXOR.java
@@ -28,3 +28,29 @@
 // 1 <= arr.length <= 300
 // 1 <= arr[i] <= 108
 
+class Solution {
+    public int countTriplets(int[] arr) {
+        
+        int count = 0;
+        int n = arr.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int a = 0;
+                for (int k = i; k < j; k++) {
+                    a ^= arr[k];
+                }
+                
+                int b = 0;
+                for (int k = j; k < n; k++) {
+                    b ^= arr[k];
+                    if (a == b) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+}
