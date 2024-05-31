bbba066f639083ab8743b6dad023297037545685
Hariprasath R <hariprasath1115@gmail.com>
2024-05-31 17:14:02 +0530

    Day 152 Done

--- Day152/SingleNumberIII.java
+++ Day152/SingleNumberIII.java
@@ -1,25 +1,52 @@
-Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+// Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
 
-You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+// You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
 
 
-Example 1:
+// Example 1:
 
-Input: nums = [1,2,1,3,2,5]
-Output: [3,5]
-Explanation:  [5, 3] is also a valid answer.
-Example 2:
+// Input: nums = [1,2,1,3,2,5]
+// Output: [3,5]
+// Explanation:  [5, 3] is also a valid answer.
+// Example 2:
 
-Input: nums = [-1,0]
-Output: [-1,0]
-Example 3:
+// Input: nums = [-1,0]
+// Output: [-1,0]
+// Example 3:
 
-Input: nums = [0,1]
-Output: [1,0]
+// Input: nums = [0,1]
+// Output: [1,0]
  
 
-Constraints:
+// Constraints:
 
-2 <= nums.length <= 3 * 104
--231 <= nums[i] <= 231 - 1
-Each integer in nums will appear twice, only two integers will appear once.
+// 2 <= nums.length <= 3 * 104
+// -231 <= nums[i] <= 231 - 1
+// Each integer in nums will appear twice, only two integers will appear once.
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        int n = nums.length;
+        int[] result = new int[2];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[index++] = nums[i];
+                if (index == 2) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
