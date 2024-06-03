339b89acff28e7bd33e5cfc4d01b7b3855f77861
Hariprasath R <hariprasath1115@gmail.com>
2024-06-03 18:13:49 +0530

    Day 155 Done

--- Day155/AppendCharactersToStringToMakeSubsequence.java
+++ Day155/AppendCharactersToStringToMakeSubsequence.java
@@ -31,3 +31,24 @@
 
 // 1 <= s.length, t.length <= 105
 // s and t consist only of lowercase English letters.
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+
+        int n = s.length();
+        int m = t.length();
+        
+        int i = 0, j = 0;
+        
+        while(i < n && j < m) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return m - j;        
+    }
+}
