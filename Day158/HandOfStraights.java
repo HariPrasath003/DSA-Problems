e663ff2e3691d95d56802405bc36c4d10aa5ffa4
Hariprasath R <hariprasath1115@gmail.com>
2024-06-06 19:13:03 +0530

    Day 158 Done

--- Day158/HandOfStraights.java
+++ Day158/HandOfStraights.java
@@ -25,3 +25,34 @@ package Day158;
 // 0 <= hand[i] <= 109
 // 1 <= groupSize <= hand.length
 
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if (hand.length % groupSize != 0) return false;
+
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int card: hand) {
+            freq.put(card, freq.getOrDefault(card, 0) + 1);
+        }
+
+        Arrays.sort(hand);
+
+        for (int i = 0; i < hand.length; i++) {
+            if (freq.get(hand[i]) == 0) {
+                continue;
+            }
+
+            for (int j = 0; j < groupSize; j++) {
+                int currCard = hand[i] + j;
+                if (freq.getOrDefault(currCard, 0) == 0) {
+                    return false;
+                }
+
+                freq.put(currCard, freq.get(currCard) - 1);
+            }
+        }
+
+        return true;
+    }
+}
