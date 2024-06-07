6eef65a3f0c1af5b4769e1577a59cc70294eef0f
Hariprasath R <hariprasath1115@gmail.com>
2024-06-07 18:28:49 +0530

    Day 159 Done

--- Day159/ReplaceWords.java
+++ Day159/ReplaceWords.java
@@ -24,3 +24,41 @@
 // Every two consecutive words in sentence will be separated by exactly one space.
 // sentence does not have leading or trailing spaces.
 
+
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        
+        String[] a = sentence.split(" ");
+        StringBuilder sb = new StringBuilder("");
+
+        for (String i: a) {
+
+            List<String> dict = new ArrayList<>();
+            for (String t: dictionary) {
+
+                if(i.startsWith(t)) {
+                    i = t;
+                    dict.add(t);
+                }
+            }
+            if(dict.size() > 1) {
+
+                int len = 1000;
+                String ans = "";
+
+                for (String z : dict) {
+
+                    if(z.length() < len) {
+                        
+                        ans = z;
+                        len = z.length();
+                    }
+                }
+                sb.append(ans + " ");
+            } else {
+                sb.append(i + " ");
+            }
+        }
+        return sb.toString().trim();
+    }
+}
