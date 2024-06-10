ddb39520c7d6d1e08a9443cb76fadbeaa632dfac
Hariprasath R <hariprasath1115@gmail.com>
2024-06-10 21:45:38 +0530

    Day 162 Done

--- Day162/HeightChecker.java
+++ Day162/HeightChecker.java
@@ -26,12 +26,12 @@
 // heights:  [1,2,3,4,5]
 // expected: [1,2,3,4,5]
 // All indices match.
- 
 
 // Constraints:
 
 // 1 <= heights.length <= 100
 // 1 <= heights[i] <= 100
+
 
 import java.util.Arrays;
 
