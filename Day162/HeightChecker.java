87b8d53ca73b59c96233e02a13aa4be56a44a205
Hariprasath R <hariprasath1115@gmail.com>
2024-06-10 21:41:35 +0530

    Day 162 Done

--- Day162/HeightChecker.java
+++ Day162/HeightChecker.java
@@ -34,3 +34,18 @@
 // 1 <= heights[i] <= 100
 
 
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i])
+                count++;
+        }
+        
+        return count;
+    }
+}
