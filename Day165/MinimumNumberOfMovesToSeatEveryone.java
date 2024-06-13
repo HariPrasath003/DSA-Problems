9b1b7f32e5fb1b9ac4d095b21b9f84f01ba19646
Hariprasath R <hariprasath1115@gmail.com>
2024-06-13 20:14:19 +0530

    Day 165 Done

--- Day165/MinimumNumberOfMovesToSeatEveryone.java
+++ Day165/MinimumNumberOfMovesToSeatEveryone.java
@@ -47,3 +47,24 @@
 // 1 <= n <= 100
 // 1 <= seats[i], students[j] <= 100
 
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+
+        int[] pos = new int[101];
+        int n = seats.length;
+
+        for(int i = 0; i < n; i++){
+            pos[seats[i]]++;
+            pos[students[i]]--;
+        }
+
+        int res = 0;
+        int current = 0;
+        
+        for(int i: pos){
+            res += Math.abs(current);
+            current += i;
+        }
+        return res;
+    }
+}
