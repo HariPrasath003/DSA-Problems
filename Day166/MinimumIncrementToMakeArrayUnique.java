72ca1820dff2581da33b7cbfc13d89ae6a66b7b0
Hariprasath R <hariprasath1115@gmail.com>
2024-06-14 20:47:07 +0530

    Day 166 Done

--- Day166/MinimumIncrementToMakeArrayUnique.java
+++ Day166/MinimumIncrementToMakeArrayUnique.java
@@ -1,7 +1,5 @@
 // You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1.
-
 // Return the minimum number of moves to make every value in nums unique.
-
 // The test cases are generated so that the answer fits in a 32-bit integer.
 
  
@@ -11,6 +9,7 @@
 // Input: nums = [1,2,2]
 // Output: 1
 // Explanation: After 1 move, the array could be [1, 2, 3].
+
 // Example 2:
 
 // Input: nums = [3,2,1,2,1,7]
@@ -23,3 +22,4 @@
 
 // 1 <= nums.length <= 105
 // 0 <= nums[i] <= 105
+
