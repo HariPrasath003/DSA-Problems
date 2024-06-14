2ccc4214797b0503e6a765dc7ace5a209e23a423
Hariprasath R <hariprasath1115@gmail.com>
2024-06-14 20:47:33 +0530

    Day 166 Done

--- Day166/MinimumIncrementToMakeArrayUnique.java
+++ Day166/MinimumIncrementToMakeArrayUnique.java
@@ -23,3 +23,22 @@
 // 1 <= nums.length <= 105
 // 0 <= nums[i] <= 105
 
+
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int numTracker = 0; 
+        int minIncrement = 0; 
+
+        for (int num : nums) {
+            
+            numTracker = Math.max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1; 
+        }
+
+        return minIncrement;
+    }
+}
