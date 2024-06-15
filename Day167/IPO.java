53e5ac493fadad025d4e275d1bba16216a058b24
Hariprasath R <hariprasath1115@gmail.com>
2024-06-15 19:53:58 +0530

    Day 167 Done

--- Day167/IPO.java
+++ Day167/IPO.java
@@ -34,8 +34,8 @@
 
 
 class Solution {
-    // Defining the Project class within the Solution class
     private static class Project {
+        
         int capital;
         int profit;
 
@@ -49,34 +49,26 @@ class Solution {
         int n = profits.length;
         List<Project> projects = new ArrayList<>();
 
-        // Creating list of projects with capital and profits
         for (int i = 0; i < n; i++) {
             projects.add(new Project(capital[i], profits[i]));
         }
 
-        // Sorting projects by capital required
         Collections.sort(projects, (a, b) -> a.capital - b.capital);
-
-        // Max-heap to store profits (using a min-heap with inverted values)
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((x, y) -> y - x);
+        
         int i = 0;
 
-        // Main loop to select up to k projects
         for (int j = 0; j < k; j++) {
-            // Add all profitable projects that we can afford
             while (i < n && projects.get(i).capital <= w) {
                 maxHeap.add(projects.get(i).profit);
                 i++;
             }
 
-        
             if (maxHeap.isEmpty()) {
                 break;
             }
-
             w += maxHeap.poll();
         }
-
         return w;
     }
 }
