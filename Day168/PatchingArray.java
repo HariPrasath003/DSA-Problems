41722e4277135533f4a70bfb3aa4a071d0af8d9b
Hariprasath R <hariprasath1115@gmail.com>
2024-06-16 19:00:28 +0530

    Day 168 Done

--- Day168/PatchingArray.java
+++ Day168/PatchingArray.java
@@ -30,3 +30,25 @@
 // 1 <= nums[i] <= 104
 // nums is sorted in ascending order.
 // 1 <= n <= 231 - 1
+
+
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        
+        long miss = 1;
+        int result = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
