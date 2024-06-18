1624e4c9ae8e07d800ea5cb24b24c3f8f44f6bb3
Hariprasath R <hariprasath1115@gmail.com>
2024-06-18 21:08:48 +0530

    Day 170 Done

--- Day170/MostProfitAssigningWork.java
+++ Day170/MostProfitAssigningWork.java
@@ -1,7 +1,6 @@
 // 826. Most Profit Assigning Work : https://leetcode.com/problems/most-profit-assigning-work/description/?envType=daily-question&envId=2024-06-18
 
 // You have n jobs and m workers. You are given three arrays: difficulty, profit, and worker where:
-
 // difficulty[i] and profit[i] are the difficulty and the profit of the ith job, and
 // worker[j] is the ability of jth worker (i.e., the jth worker can only complete a job with difficulty at most worker[j]).
 // Every worker can be assigned at most one job, but one job can be completed multiple times.
@@ -16,6 +15,7 @@
 // Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
 // Output: 100
 // Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get a profit of [20,20,30,30] separately.
+
 // Example 2:
 
 // Input: difficulty = [85,47,57], profit = [24,66,99], worker = [40,25,25]
