21b8df16a1d9ee7439dfc2ed838770c0b93395a7
Hariprasath R <hariprasath1115@gmail.com>
2024-06-21 19:36:29 +0530

    Day 173 Done

--- Day173/GrumpyBookstoreOwner.java
+++ Day173/GrumpyBookstoreOwner.java
@@ -31,3 +31,29 @@
 // 1 <= minutes <= n <= 2 * 104
 // 0 <= customers[i] <= 1000
 // grumpy[i] is either 0 or 1.
+
+
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+
+        int n = grumpy.length;
+        int i = 0;
+        int curWin = 0;
+        int maxWin = 0;
+
+        for(int j=0;j<n;j++) {
+            curWin+=customers[j] * grumpy[j];
+            maxWin = Math.max(curWin, maxWin);
+            
+            if(j >= minutes - 1 ) {
+                curWin -= customers[i] * grumpy[i++];
+            }
+        }
+        int res = maxWin;
+        for(i=0;i<grumpy.length;i++) {
+            res+=customers[i] * (1 - grumpy[i]);
+        }
+        return res;
+
+    }
+}
