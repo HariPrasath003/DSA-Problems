70c21809a95c85f082a80d752bb861b1df4e4365
Hariprasath R <hariprasath1115@gmail.com>
2024-06-23 18:53:06 +0530

    Day 175 Done

--- Day175/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
+++ Day175/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -1,9 +1,8 @@
 // 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit : https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/description/?envType=daily-question&envId=2024-06-23
 
 
-// Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
-
- 
+// Given an array of integers nums and an integer limit, 
+// return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
 // Example 1:
 
@@ -21,11 +20,13 @@
 // [4,7] with maximum absolute diff |4-7| = 3 <= 4.
 // [7] with maximum absolute diff |7-7| = 0 <= 4. 
 // Therefore, the size of the longest subarray is 2.
+
 // Example 2:
 
 // Input: nums = [10,1,2,4,7,2], limit = 5
 // Output: 4 
 // Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+
 // Example 3:
 
 // Input: nums = [4,2,2,2,4,4,2,2], limit = 0
@@ -37,3 +38,4 @@
 // 1 <= nums.length <= 105
 // 1 <= nums[i] <= 109
 // 0 <= limit <= 109
+
