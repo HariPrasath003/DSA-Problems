a369510b2bce3548de6ebfd986f0c1fc485ba087
Hariprasath R <hariprasath1115@gmail.com>
2024-06-28 22:31:53 +0530

    Day 180 Done

--- Day180/MaximumTotalImportanceOfRoads.java
+++ Day180/MaximumTotalImportanceOfRoads.java
@@ -42,3 +42,33 @@
 // 0 <= ai, bi <= n - 1
 // ai != bi
 // There are no duplicate roads.
+
+
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+
+        int[] degree = new int[n];
+        
+        for (int[] road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+        
+        Integer[] cities = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            cities[i] = i;
+        }
+        Arrays.sort(cities, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(degree[b], degree[a]);
+            }
+        });
+        
+        long totalImportance = 0;
+        for (int i = 0; i < n; i++) {
+            totalImportance += (long) (n - i) * degree[cities[i]];
+        }
+        
+        return totalImportance;
+    }
+}
