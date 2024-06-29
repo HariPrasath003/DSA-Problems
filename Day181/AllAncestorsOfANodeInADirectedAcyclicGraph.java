b81b012f6bd4acd9677b7d347e2491d32e504d94
Hariprasath R <hariprasath1115@gmail.com>
2024-06-29 11:36:47 +0530

    Day 181 Done

--- Day181/AllAncestorsOfANodeInADirectedAcyclicGraph.java
+++ Day181/AllAncestorsOfANodeInADirectedAcyclicGraph.java
@@ -2,11 +2,8 @@
 
 
 // You are given a positive integer n representing the number of nodes of a Directed Acyclic Graph (DAG). The nodes are numbered from 0 to n - 1 (inclusive).
-
 // You are also given a 2D integer array edges, where edges[i] = [fromi, toi] denotes that there is a unidirectional edge from fromi to toi in the graph.
-
 // Return a list answer, where answer[i] is the list of ancestors of the ith node, sorted in ascending order.
-
 // A node u is an ancestor of another node v if u can reach v via a set of edges.
 
  
@@ -24,6 +21,7 @@
 // - Node 5 has three ancestors 0, 1, and 3.
 // - Node 6 has five ancestors 0, 1, 2, 3, and 4.
 // - Node 7 has four ancestors 0, 1, 2, and 3.
+
 // Example 2:
 
 
@@ -47,3 +45,4 @@
 // fromi != toi
 // There are no duplicate edges.
 // The graph is directed and acyclic.
+
