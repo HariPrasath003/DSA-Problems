b7215ebe0f44bf8af6c71e98e59db427081e8586
Hariprasath R <hariprasath1115@gmail.com>
2024-06-30 18:50:05 +0530

    Day 182 Done

--- Day182/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
+++ Day182/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
@@ -1,3 +1,5 @@
+package Day182;
+
 // 1579. Remove Max Number of Edges to Keep Graph Fully Traversable : https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/description/?envType=daily-question&envId=2024-06-30
 
 
