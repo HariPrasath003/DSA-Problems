d72cb9fe9beaa2765bbe9849b2a8635eb2b53895
Hariprasath R <hariprasath1115@gmail.com>
2024-07-01 18:01:17 +0530

    Day 183 Done

--- Day183/ThreeConsecutiveOdds.java
+++ Day183/ThreeConsecutiveOdds.java
@@ -19,3 +19,23 @@
 
 // 1 <= arr.length <= 1000
 // 1 <= arr[i] <= 1000
+
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+
+        int count  = 0;
+        for(int i=0; i<arr.length; i++){
+
+            if(arr[i]%2 == 0){
+                count = 0;
+            }
+            else count++;
+
+            if(count == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
