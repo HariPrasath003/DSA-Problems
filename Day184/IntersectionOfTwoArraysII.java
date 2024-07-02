9651f68c9244ec56e6ce6a60d5ca7928ba78a2e9
Hariprasath R <hariprasath1115@gmail.com>
2024-07-02 15:47:09 +0530

    Day 184 Done

--- Day184/IntersectionOfTwoArraysII.java
+++ Day184/IntersectionOfTwoArraysII.java
@@ -25,3 +25,33 @@
 // What if nums1's size is small compared to nums2's size? Which algorithm is better?
 // What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0, j = 0, k = 0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        while( i < l1 && j < l2)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1,0,k);
+    }
+}
+
