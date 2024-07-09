24f4eedd4a98e5c70edc56c85301788b0e56002f
Hariprasath R <hariprasath1115@gmail.com>
2024-07-09 19:47:17 +0530

    Day 191 Done

--- Day191/AverageWaitingTime.java
+++ Day191/AverageWaitingTime.java
@@ -4,14 +4,11 @@ package Day191;
 
 
 // There is a restaurant with a single chef. You are given an array customers, where customers[i] = [arrivali, timei]:
-
 // arrivali is the arrival time of the ith customer. The arrival times are sorted in non-decreasing order.
 // timei is the time needed to prepare the order of the ith customer.
 // When a customer arrives, he gives the chef his order, and the chef starts preparing it once he is idle. The customer waits till the chef finishes preparing his order. The chef does not prepare food for more than one customer at a time. The chef prepares food for customers in the order they were given in the input.
-
 // Return the average waiting time of all customers. Solutions within 10-5 from the actual answer are considered accepted.
 
- 
 
 // Example 1:
 
@@ -22,6 +19,7 @@ package Day191;
 // 2) The second customer arrives at time 2, the chef takes his order and starts preparing it at time 3, and finishes at time 8, so the waiting time of the second customer is 8 - 2 = 6.
 // 3) The third customer arrives at time 4, the chef takes his order and starts preparing it at time 8, and finishes at time 11, so the waiting time of the third customer is 11 - 4 = 7.
 // So the average waiting time = (2 + 6 + 7) / 3 = 5.
+
 // Example 2:
 
 // Input: customers = [[5,2],[5,4],[10,3],[20,1]]
