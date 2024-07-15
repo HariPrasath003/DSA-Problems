42af2493a1ba1c530592197c8e3ef34a4ed991a0
Hariprasath R <hariprasath1115@gmail.com>
2024-07-15 19:23:23 +0530

    Day 197 Done

--- Day197/CreateBinaryTreeFromDescriptions.java
+++ Day197/CreateBinaryTreeFromDescriptions.java
@@ -2,13 +2,10 @@
 
 
 // You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
-
 // If isLefti == 1, then childi is the left child of parenti.
 // If isLefti == 0, then childi is the right child of parenti.
 // Construct the binary tree described by descriptions and return its root.
-
 // The test cases will be generated such that the binary tree is valid.
-
  
 
 // Example 1:
@@ -18,6 +15,8 @@
 // Output: [50,20,80,15,17,19]
 // Explanation: The root node is the node with value 50 since it has no parent.
 // The resulting binary tree is shown in the diagram.
+
+
 // Example 2:
 
 
